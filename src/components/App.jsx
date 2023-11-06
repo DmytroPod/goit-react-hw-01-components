e6ecc53adbdf194f299fc055103c68c8c3001156
DmytroPod <132753424+DmytroPod@users.noreply.github.com>
2023-11-06 14:20:28 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import React from 'react';
+import { Profile } from './Profile/Profile';
+import { user } from 'data';
+import { user } from 'data';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
   );
 };
